1b8f4d14ba49b5f98778bd5d920035513dbc641b
henry <henrychao4@gmail.com>
2024-02-29 13:46:27 -0500

    2.29

--- tradeofftest.R
+++ tradeofftest.R
@@ -18,15 +18,15 @@ theme_update(
 
 set.seed(1)
 
-nspec = 3
+nspec = 2
 C = seq(0,1, length.out = nspec)
 
 params = list(
   nspec = nspec,
   inflow = 1,
   outflow = .1,
-  C = C,
-  death = .05 * C
+  C = c(1,.499),
+  death = c(.2, .1)
 )
 
 init_state = c(10, rep(10, params$nspec))
@@ -40,7 +40,14 @@ model =
     return(list(c(dRdt,dNdt)))
   }
 
-sim = ode(y = init_state, times = seq(0, 1000, by = 1), func = model, parms = params)
+sim = ode(y = init_state, times = seq(0, 1000, by = 1), func = model, parms = params) |>
+  as.data.frame()
+colnames(sim) = c('time', 'R', 'N1', 'N2')
+
+min(filter(sim, N2 < 1)$time)
+
+
+exclusion_time = 0
 
 sim.df = as.data.frame(sim)
 spec.abuns = sim.df[-2]
