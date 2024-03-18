6fe48810e9fb818f59f6ceff625597a9128299c7
henry <henrychao4@gmail.com>
2024-03-18 19:14:46 -0400

    3.18

--- exclusiontimes_2res.R
+++ exclusiontimes_2res.R
@@ -35,13 +35,13 @@ simulation =
     
     inv_d = inv_c1 * (res_C[2,2] * res_d[1] - res_C[1,2] * res_d[2]) / (res_C[1,1] * res_C[2,2] - res_C[1,2] * res_C[2,1]) + inv_c2 * (res_C[1,1] * res_d[2] - res_C[2,1] * res_d[1]) / (res_C[1,1] * res_C[2,2] - res_C[1,2] * res_C[2,1])
     
-    # pert_inv_c1 = runif(1, min = 0, max = .01)
-    # pert_inv_c2 = runif(1, min = 0, max = .01)
-    # pert_inv_d = runif(1, min = 0, max = .01)
+    pert_inv_c1 = runif(1, min = 0, max = .01)
+    pert_inv_c2 = runif(1, min = 0, max = .01)
+    pert_inv_d = runif(1, min = 0, max = .01)
     
-    pert_inv_c1 = .000
-    pert_inv_c2 = .000
-    pert_inv_d = .000
+    # pert_inv_c1 = .01
+    # pert_inv_c2 = .01
+    # pert_inv_d = .01
     
     inv_c1 = inv_c1 - pert_inv_c1
     inv_c2 = inv_c2 - pert_inv_c2
@@ -51,9 +51,18 @@ simulation =
     C = rbind(res_C, inv_C)
     d = c(res_d, inv_d)
     
-    init_state = c(rep(10, length(r)), rep(10, length(res_d)), rep(10, length(inv_d)))
+    init_residents = 0
+    init_resources = 0
+    init_invader = 1
     
-    sim = ode(y = init_state, times = seq(0, 3000, by = .1), func = model, parms = list(r = r, K = K, C = C, d = d)) |>
+    init_state = c(rep(10, length(r)), rep(10, length(res_d)))
+    initialize = ode(y = init_state, times = seq(0, 3000, by = .1), func = model, parms = list(r = r, K = K, C = res_C, d = res_d))
+    resident_eql = tail(initialize, 1)[-1]
+    invader_init = .1
+    
+    new_init_state = c(resident_eql, invader_init)
+    
+    sim = ode(y = new_init_state, times = seq(0, 3000, by = .1), func = model, parms = list(r = r, K = K, C = C, d = d)) |>
       as.data.frame()
     
     colnames(sim) = c('time', 'R1', 'R2', 'N1', 'N2', 'N3')
@@ -75,8 +84,8 @@ simulation =
   }
 
 params = expand_grid(
-  inv_c1 = seq(.1, .3, by = .05),
-  inv_c2 = seq(.1, .3, by = .05)
+  inv_c1 = seq(.1, .3, by = .01),
+  inv_c2 = seq(.1, .3, by = .01)
 )
 
 results =
@@ -98,13 +107,13 @@ neutral_plane = function(plot_c31, plot_c32){
   return(d3)
 }
 
-plot_c31 = seq(.05, .25, by = .01)
-plot_c32 = seq(.05, .25, by = .01)
+plot_c31 = seq(.1, .3, by = .01)
+plot_c32 = seq(.1, .3, by = .01)
 plot_d = outer(plot_c31, plot_c32, neutral_plane)
 
 p2 = plot_ly(data = results, x = ~c_31, y = ~c_32, z = ~d_3, type = "scatter3d", mode = "markers", color = ~Exclusion_Time) %>%
   add_trace(x = .2, y = .1, z = .2, inherit = FALSE) %>%
   add_trace(x = .1, y = .2, z = .2, color = 'resident', inherit = FALSE) %>%
-  add_surface(x = plot_c31, y = plot_c32, z = plot_d, inherit = FALSE)
+  add_surface(x = plot_c31, y = plot_c32, z = plot_d, color = 'resident', inherit = FALSE)
 
 print(p2)
