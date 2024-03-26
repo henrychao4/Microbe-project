0588a8c9d6c61243b6d35a7bb6072b1ab1a6a9b2
henry <henrychao4@gmail.com>
2024-03-26 13:15:18 -0400

    3.26

--- extinctiontest.R
+++ extinctiontest.R
@@ -23,7 +23,7 @@ nresident = 2
 ninvader = 1
 nresource = 2
 res_c = matrix(c(.2, .1, .1, .2), nrow = nresident, ncol = nresource)
-inv_c = c(.4, .2)
+inv_c = c(.4, .3)
 
 res_d = c(.2, .2)
 inv_d = inv_c[1] * (res_c[2,2] * res_d[1] - res_c[1,2] * res_d[2]) / (res_c[1,1] * res_c[2,2] - res_c[1,2] * res_c[2,1]) + inv_c[2] * (res_c[1,1] * res_d[2] - res_c[2,1] * res_d[1]) / (res_c[1,1] * res_c[2,2] - res_c[1,2] * res_c[2,1])
@@ -40,9 +40,15 @@ params = list(
   d = d
 )
 
-init_state = c(rep(10, nresource), rep(10, nresident), rep(10, ninvader))
+init_state = c(rep(10, length(r)), rep(10, length(res_d)))
+initialize = ode(y = init_state, times = seq(0, 10000, by = .1), func = model, parms = list(r = r, K = K, C = res_c, d = res_d))
+resident_eql = tail(initialize, 1)[-1]
 
-sim = ode(y = init_state, times = seq(0, 5000, by = .1), func = model, parms = params) |>
+
+invader_init = 1
+new_init_state = c(resident_eql, invader_init)
+
+sim = ode(y = new_init_state, times = seq(0, 1000, by = .1), func = model, parms = params) |>
   as.data.frame()
 
 colnames(sim) = c('time', 'R1', 'R2', 'Nres1', 'Nres2', paste0('N', 1:ninvader))
