91d87594f038b2653491e009f5f9047ce78901e4
henrychao4 <149727372+henrychao4@users.noreply.github.com>
2024-04-25 14:48:13 -0400

    Update model_current.R

--- Model Code/model_current.R
+++ Model Code/model_current.R
@@ -16,76 +16,113 @@ theme_update(
   aspect.ratio = 1
 )
 
-nspec = 5
-nres = 5
+set.seed(4)
 
-resources = seq(0, 1, length = nres)
-traits = seq(0, 1, length = nspec)
-dists = outer(resources, traits, FUN = \(x, y) abs(x - y))
-dists[dists > .5] = 1 - dists[dists > .5]
-C = exp(-(dists / .5) ^ 2)
+nspec = 10
+nres = 10
 
 params = list(
   nspec = nspec,
   nres = nres,
-  immigration = rep(.1, nspec),
-  death = rep(.1, nspec),
-  inflow = rep(.1, nres),
-  outflow = rep(.05, nres),
-  assimilation = matrix(.8, nrow = nspec, ncol = nres),
-  conversion = C,
-  max_growth = matrix(.1, nrow = nspec, ncol = nres),
-  half_saturation = matrix(.1, nrow = nspec, ncol = nres),
-  byproduct = array(rep(.1, nspec * nres^2), dim=c(nspec, nres, nres))
+  alpha = rep(0, nspec),
+  mu = rep(.05, nspec),
+  rho = rep(.3, nres),
+  delta = rep(.05, nres),
+  epsilon = .9,
+  beta = 1,
+  h = 1,
+  #I = diag(nspec),
+  I = matrix(rbinom(nspec * nres, 1, 0.5), nrow = nspec, ncol = nres),
+  p = array(rep(0, nspec * nres^2), dim=c(nspec, nres, nres))
 )
 
-init_state = c(rep(10, params$nspec), rep(5, params$nres))
+init_state = c(rep(1, params$nspec), rep(1, params$nres), 0)
 
-mult = 
-  \(params){
-    ans = sapply(1:nrow(params$conversion), function(i) params$conversion[i,] %*% t(params$byproduct[i,,]))
-    return(t(ans))
-  }
-
-index = 
-  \(res){
-    return(1)
-  }
-
-consumption = 
+U = 
   \(params, R){
-    ans = params$max_growth * (R / (params$half_saturation + R))
-    return(ans)
-  }
-
-res_production = 
-  \(params, R){
-    ans = matrix(0,nrow(consumption(params, R)),ncol(consumption(params, R)))
-    for (i in 1:nrow(consumption(params, R))) {
-      ans[i,] = consumption(params, R)[i,] %*% params$byproduct[i,,]
+    ans = matrix(0, nrow = params$nspec, ncol = params$nres)
+    for (j in 1:params$nres) {
+      for (i in 1:params$nspec) {
+        ans[i,j] = params$beta * R[j] / (params$h + R[j]) * params$I[i,j]
+      }
     }
     return(ans)
   }
 
-growth = 
+omega = 
   \(params, R){
-    ans = rowSums((params$conversion * params$assimilation - mult(params)) * consumption(params, R))
+    ans = matrix(0, nrow = params$nspec, ncol = params$nres)
+    for (i in 1:params$nspec) {
+      ans[i,] = U(params, R)[i,] %*% params$p[i,,]
+    }
     return(ans)
+  }
+
+gamma = 
+  \(params, R){
+    ans = rep(0, params$nspec)
+    for (i in 1:params$nspec) {
+      ans[i] = params$epsilon * sum(U(params, R)[i,])
+    }
+    return(ans)
+  }
+
+find_num_alpha =
+  \(eql, eql_abuns, model, params){
+    dt = .001
+    dNj = .001
+    fwd_result = ode(y = eql, times = seq(0, dt, by = dt/100), func = model, parms = params)
+    fwd_state = as.numeric(tail(fwd_result, n = 1)[-1])
+    fwd_abuns = fwd_state[0:nspec]
+    num_alpha = matrix(0, params$nspec, params$nspec)
+    for (j in 1:nspec) {
+      pert_state = eql
+      pert_state[j] = pert_state[j] + dNj
+      pert_abuns = pert_state[0:nspec]
+      fwd_pert_result = ode(y = pert_state, times = seq(0,dt, by = dt/100), func = model, parms = params)
+      fwd_pert_state = as.numeric(tail(fwd_pert_result, n = 1)[-1])
+      fwd_pert_abuns = fwd_pert_state[0:nspec]
+      num_alpha[,j] = (fwd_pert_abuns - pert_abuns) / (dt * dNj * pert_abuns) - (fwd_abuns - eql_abuns) / (dt * dNj * eql_abuns)
+    }
+    return(num_alpha)
   }
 
 model = 
   \(t, state, params){
     N = state[1:params$nspec]
-    R = state[-(1:params$nspec)]
-    dNdt = params$immigration + growth(params, R) * N - params$death * N
-    dRdt = params$inflow - params$outflow * R + colSums(res_production(params, R)) - colSums(consumption(params, R) * N)
-    return(list(c(dNdt,dRdt)))
+    R = state[(params$nspec + 1):(params$nspec + params$nres)]
+    W = tail(state, 1)
+    dNdt = params$alpha + gamma(params, R) * N - params$mu * N
+    dRdt = params$rho - params$delta * R + colSums(omega(params, R)) - colSums(U(params, R) * N)
+    dWdt = (1 - params$epsilon) * sum(U(params, R) * N)
+    
+    return(list(c(dNdt, dRdt, dWdt)))
   }
 
-sim = ode(y = init_state, times = seq(0, 100, by = 1), func = model, parms = params)
+sim = ode(y = init_state, times = seq(0, 5000, by = 1), func = model, parms = params)
 
 sim.df = as.data.frame(sim)
-spec.abuns = sim.df[-((nspec+2):(nspec + nres + 1))]
+spec.abuns = sim.df[-((nspec+2):(nspec + nres + 2))]
 abuns.df = melt(spec.abuns, id.vars='time')
 p <- ggplot(abuns.df, aes(time, value, color = variable)) + geom_line() + theme_classic()
 print(p)
+
+eql = tail(sim, 1)[-1]
+eql_abuns = eql[0:nspec]
+num_coexist = length(eql_abuns[eql_abuns > .1])
+df = c(eql_abuns, num_coexist) |> t() |> as_tibble()
+colnames(df) = c(paste0('N', seq(nspec)), 'num_coexist')
+
+hamming_dist = \(x,y) {
+  ans = sum(x != y)
+  return(ans)
+}
+
+dists = matrix(0, nrow = nspec, ncol = nspec)
+for (i in 1:nspec) {
+  for (j in 1:nspec) {
+    dists[i,j] = hamming_dist(params$I[i,], params$I[j,])
+  }
+}
+
+num_alpha = find_num_alpha(eql, eql_abuns, model, params)
