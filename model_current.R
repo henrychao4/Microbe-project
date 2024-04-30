9db37bf275b13e08325ad21fed25ebf2527efc27
Henry Chao <henry.chao@stonybrook.edu>
2024-04-30 14:53:25 -0400

    4.30

---
+++ model_current.R
@@ -0,0 +1,154 @@
+library(ggplot2)
+library(tensor)
+library(future)
+library(parallel)
+library(deSolve)
+library(tibble)
+library(tidyr)
+library(dplyr)
+library(reshape2)
+
+plan(multisession(workers = detectCores() - 2))
+theme_set(theme_bw())
+theme_update(
+  panel.grid = element_blank(),
+  strip.background = element_rect(fill = 'orange'),
+  aspect.ratio = 1
+)
+
+set.seed(1)
+
+nspec = 20
+nres = 20
+
+makeI = \(nspec, nres) {
+  I = matrix(0, nrow = nspec, ncol = nres)
+  cons_vec = c(rep(0, nres/2), rep(1, nres/2))
+  for (i in 1:nspec) {
+    I[i,] = sample(cons_vec, nres, replace = F)
+  }
+  return(I)
+}
+
+params = list(
+  nspec = nspec,
+  nres = nres,
+  alpha = rep(0, nspec),
+  mu = rep(.05, nspec),
+  rho = rep(.3, nres),
+  delta = rep(.05, nres),
+  # mu = rep(0, nspec),
+  # rho = rep(0, nres),
+  # delta = rep(0, nres),
+  epsilon = .9,
+  beta = 1,
+  h = 1,
+  I = makeI(nspec, nres),
+  p = array(rep(.01, nspec * nres^2), dim=c(nspec, nres, nres))
+)
+
+init_state = c(rep(1, params$nspec), rep(1, params$nres), 0)
+
+U = 
+  \(params, R){
+    ans = t(params$beta * R / (params$h + R) * t(params$I))
+    return(ans)
+  }
+
+omega = 
+  \(params, R){
+    ans = rowSums(params$p, dims = 2) * U(params, R)
+    return(ans)
+  }
+
+gamma = 
+  \(params, R){
+    ans = params$epsilon * rowSums((1 - rowSums(params$p, dims = 2)) * U(params, R))
+    return(ans)
+  }
+
+model = 
+  \(t, state, params){
+    N = state[1:params$nspec]
+    R = state[(params$nspec + 1):(params$nspec + params$nres)]
+    W = tail(state, 1)
+    dNdt = params$alpha + gamma(params, R) * N - params$mu * N
+    dRdt = params$rho - params$delta * R + colSums(omega(params, R) * N) - colSums(U(params, R) * N)
+    dWdt = sum((1 - params$epsilon) * (1 - rowSums(params$p, dims = 2)) * U(params, R) * N)
+    
+    return(list(c(dNdt, dRdt, dWdt)))
+  }
+
+find_num_alpha =
+  \(eql, eql_abuns, model, params){
+    dt = .001
+    dNj = .001
+    fwd_result = ode(y = eql, times = seq(0, dt, by = dt/100), func = model, parms = params)
+    fwd_state = as.numeric(tail(fwd_result, n = 1)[-1])
+    fwd_abuns = fwd_state[0:nspec]
+    num_alpha = matrix(0, params$nspec, params$nspec)
+    for (j in 1:nspec) {
+      pert_state = eql
+      pert_state[j] = pert_state[j] + dNj
+      pert_abuns = pert_state[0:nspec]
+      fwd_pert_result = ode(y = pert_state, times = seq(0,dt, by = dt/100), func = model, parms = params)
+      fwd_pert_state = as.numeric(tail(fwd_pert_result, n = 1)[-1])
+      fwd_pert_abuns = fwd_pert_state[0:nspec]
+      num_alpha[,j] = (fwd_pert_abuns - pert_abuns) / (dt * dNj * pert_abuns) - (fwd_abuns - eql_abuns) / (dt * dNj * eql_abuns)
+    }
+    return(num_alpha)
+  }
+
+modularity = \(I) {
+  
+}
+
+sim = ode(y = init_state, times = seq(0, 200000, by = 1), func = model, parms = params)
+
+sim.df = as.data.frame(sim)
+spec.abuns = sim.df[-((nspec+2):(nspec + nres + 2))]
+abuns.df = melt(spec.abuns, id.vars='time')
+p <- ggplot(abuns.df, aes(time, value, color = variable)) + geom_line() + theme_classic()
+print(p)
+
+eql = tail(sim, 1)[-1]
+eql_abuns = eql[0:nspec]
+num_coexist = length(eql_abuns[eql_abuns > .1])
+df = c(eql_abuns, num_coexist) |> t() |> as_tibble()
+colnames(df) = c(paste0('N', seq(nspec)), 'num_coexist')
+
+hamming_dist = \(x, y) {
+  ans = sum(x != y) / length(x)
+  return(ans)
+}
+
+dists = matrix(0, nrow = nspec, ncol = nspec)
+for (i in 1:nspec) {
+  for (j in 1:nspec) {
+    dists[i,j] = hamming_dist(params$I[i,], params$I[j,])
+  }
+}
+
+num_alpha = find_num_alpha(eql, eql_abuns, model, params)
+coexist_traits = params$I[eql_abuns > .1, eql_abuns > .1]
+
+overall_dists = matrix(0, nrow = nspec, ncol = nspec)
+coex_dists = matrix(0, nrow = nrow(coexist_traits), ncol = nrow(coexist_traits))
+
+for (i in 1:nspec) {
+  for (j in 1:nspec) {
+    overall_dists[i,j] = hamming_dist(params$I[i,], params$I[j,])
+  }
+}
+
+for (i in 1:nrow(coexist_traits)) {
+  for (j in 1:nrow(coexist_traits)) {
+    coex_dists[i,j] = hamming_dist(coexist_traits[i,], coexist_traits[j,])
+  }
+}
+
+overall_avg_ham_dist = mean(dists)
+coexist_avg_ham_dist = mean(coex_dists)
+
+print(overall_avg_ham_dist)
+print(coexist_avg_ham_dist)
---
+++ parallelsim.R
@@ -0,0 +1,144 @@
+library(tidyverse)
+library(deSolve)
+library(parallel)
+library(furrr)
+library(matlib)
+library(bipartite)
+
+plan(multisession(workers = detectCores() - 2))
+theme_set(theme_bw())
+theme_update(
+  panel.grid = element_blank(),
+  strip.background = element_rect(fill = 'orange'),
+  aspect.ratio = 1
+)
+
+nspec = 10
+nres = 10
+
+init_state = c(rep(1, nspec), rep(1, nres), 0)
+
+makeI = \(nspec, nres) {
+  I = matrix(0, nrow = nspec, ncol = nres)
+  cons_vec = c(rep(0, nres/2), rep(1, nres/2))
+  for (i in 1:nspec) {
+    I[i,] = sample(cons_vec, nres, replace = F)
+  }
+  return(I)
+}
+
+U = 
+  \(params, R){
+    ans = t(params$beta * R / (params$h + R) * t(params$I))
+    return(ans)
+  }
+
+omega = 
+  \(params, R){
+    ans = rowSums(params$p, dims = 2) * U(params, R)
+    return(ans)
+  }
+
+gamma = 
+  \(params, R){
+    ans = params$epsilon * rowSums((1 - rowSums(params$p, dims = 2)) * U(params, R))
+    return(ans)
+  }
+
+model = 
+  \(t, state, params){
+    N = state[1:params$nspec]
+    R = state[(params$nspec + 1):(params$nspec + params$nres)]
+    W = tail(state, 1)
+    dNdt = params$alpha + gamma(params, R) * N - params$mu * N
+    dRdt = params$rho - params$delta * R + colSums(omega(params, R) * N) - colSums(U(params, R) * N)
+    dWdt = sum((1 - params$epsilon) * (1 - rowSums(params$p, dims = 2)) * U(params, R) * N)
+    return(list(c(dNdt, dRdt, dWdt)))
+  }
+
+hamming_dist = \(x,y) {
+  ans = sum(x != y) / length(x)
+  return(ans)
+}
+
+simulation = 
+  \(
+    numsim,
+    nspec,
+    nres
+  ){
+    set.seed(numsim)
+    
+    alpha = 0
+    mu = .05
+    rho = .3
+    delta = .05
+    epsilon = .9
+    beta = .1
+    h = 1
+    I = makeI(nspec, nres)
+    p = array(rep(0, nspec * nres^2), dim=c(nspec, nres, nres))
+    
+    init_state = c(rep(1, nspec), rep(1, nres), 0)
+    sim = ode(y = init_state, times = seq(0, 10000, by = 1), func = model, parms = list(nspec = nspec, nres = nres, alpha = alpha, mu = mu, rho = rho, delta = delta, epsilon = epsilon, beta = beta, h = h, I = I, p = p))
+    
+    eql = tail(sim, 1)[-1]
+    eql_abuns = eql[0:nspec]
+    num_coexist = length(eql_abuns[eql_abuns > .1])
+    df = c(eql_abuns, num_coexist) |> t()
+    colnames(df) = c(paste0('N', seq(nspec)), 'num_coexist')
+    df = as_tibble(df)
+    df$I = list(I)
+    
+    overall_dists = matrix(0, nrow = nspec, ncol = nspec)
+    
+    for (i in 1:nspec) {
+      for (j in 1:nspec) {
+        overall_dists[i,j] = hamming_dist(I[i,], I[j,])
+      }
+    }
+    
+    coex_dists = 
+    
+    df$avg_ham = mean(overall_dists)
+    df$avg_coex_ham = 
+    df$modularity = DIRT_LPA_wb_plus(I)$modularity
+    df$nestedness = nested(I)
+    df$coexist_traits = as.list(I[eql_abuns > .1,eql_abuns > .1])
+
+    
+    
+    
+    return(df)
+  }
+
+params = 
+  expand_grid(
+    numsim = 1:10,
+    nspec = nspec,
+    nres = nres
+  )
+
+result =
+  params |>
+  future_pmap_dfr(
+    .f = simulation,
+    .options = furrr_options(seed = NULL)
+  )
+
+hist(result$num_coexist)
+
+ham_vec = rep(0, nspec)
+mod_vec = rep(0, nspec)
+nest_vec = rep(0, nspec)
+for (i in 1:nspec) {
+  ham_vec[i] = mean(result[result$num_coexist == i,]$avg_ham)
+  mod_vec[i] = mean(result[result$num_coexist == i,]$modularity)
+  nest_vec[i] = mean(result[result$num_coexist == i,]$nestedness)
+}
+
+
+
+plot(1:nspec, ham_vec, xlab = 'Number of Coexisting Species', ylab = 'Average Pairwise Hamming Distance')
+plot(1:nspec, mod_vec, xlab = 'Number of Coexisting Species', ylab = 'Average Modularity')
+plot(1:nspec, nest_vec, xlab = 'Number of Coexisting Species', ylab = 'Average Nestedness')
